8d802cb491099af5ddbc9fd9cfa365997e76fcb6
Javier Chavez Manzo <javtychm@gmail.com>
2020-08-21 11:06:11 -0500

    using os.mkdir, oslistdir, os.rename(move)

---
+++ Working_files/organize_photos.py
@@ -0,0 +1,7 @@
+# 1. Get a list of the file names
+# 2. Extract the place names from the file names
+# 3. Make a directory for each place name
+# 4. Move files into the right directories
+# os.mkdir Make a new directory, os.listdir List files in a directory, os.rename Move a file from a directory to another
+
+import os
