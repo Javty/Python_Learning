a68eecc1ddc9e78f8e6786c59088cd2da9f551d3
Javier Chavez Manzo <javtychm@gmail.com>
2020-08-21 15:32:51 -0500

    Finished the program for to organize photos

--- Working_files/organize_photos_complete.py
+++ Working_files/organize_photos_complete.py
@@ -1,27 +1,29 @@
 import os
+
+def organize_photos(directory):
 # 1. Get a list of the file names
-os.chdir("Photos")
-originals = os.listdir()
-places=[]
+    os.chdir(directory)
+    originals = os.listdir()
+    places=[]
 # 2. Extract the place names from the file names
-
-def extract_place3(filename):
-    return filename.split("_")[1]
-
+    for filename in originals:
+        place = extract_place(filename)
+        if place not in places:
+            places.append(place)
 # 3. Make a directory for each place name
+    make_directories(places)
+# 4. Move files into the right directories
+    for filename in originals:
+        place = extract_place(filename)
+        os.rename(filename, os.path.join(place,filename))
+
+def extract_place(filename):
+    return filename.split("_")[1]
 
 def make_directories(place):
     for x in place:
         os.mkdir(x)
 
 
-
-for x in range(len(originals)):
-    place = extract_place3(originals[x])
-    if place not in places:
-        places.append(place)
-
-make_directories(places)
-print(os.listdir())
-
-# 4. Move files into the right directories
+if __name__ == '__main__':
+    organize_photos("Photos")
