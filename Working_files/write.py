6ef03d42a80fc51769547b973803d826446e5631
Javier Chavez Manzo <javtychm@gmail.com>
2020-08-28 12:34:13 -0500

    Cambios para Marianne

--- Working_files/write.py
+++ Working_files/write.py
@@ -3,7 +3,7 @@
 # Write to the file
 
 
-def copy_files()
+def copy_files():
 # Copy contents of one file to another
     with open("read.txt") as reader:
         with open("copy.txt") as copy:
@@ -12,7 +12,7 @@ def copy_files()
 def write_numbers(numbers):
     with open("file_numbers.txt", "w") as my_story:
         for number in range(numbers):
-            if number % 2 == 0:
+            if number % 2 != 0:
                 my_story.write(f"{number}\n")
 
 if __name__ == '__main__':
