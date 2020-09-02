275351ffdf7f870ba106ea476e63a508e2a78c27
Javier Chavez Manzo <javtychm@gmail.com>
2020-09-01 21:44:03 -0500

    Animal Classes

---
+++ Objects_Classes/animals.py
@@ -0,0 +1,69 @@
+class Dog:
+
+    scientific_name = "Canis lupus familiaris"
+
+    def __init__(self, name):
+        self.name = name
+        self.woofs = 0
+
+    def do_trick(self):
+        pass
+
+    def count(self):
+        self.woofs += 1
+        for bark in range(self.woofs):
+            self.speak()
+
+
+    def speak(self):
+        print("Woof!")
+
+    def eat(self, food):
+        if food == "biscuit":
+            print("Yummy!")
+        else:
+            print("That's not food!")
+
+    def hear(self, words):
+        if self.name in words:
+            self.speak()
+        else:
+            print("This string does not have the dog's name in it.")
+
+class Chihuahua(Dog):
+    origin = "Mexico"
+
+    def speak(self):
+        print("Yip!")
+
+class Husky(Dog):
+    origin = "Mexico"
+
+    def speak(self):
+        print("Awooooo!")
+
+class Viejo_pastor_ingles():
+    pass
+
+class TrainedChihuahua(Chihuahua):
+    def do_trick(self):
+        print("The chihuahua spins in the air and turns briefly into a chicken.")
+
+
+
+
+
+
+
+class Cat:
+
+    def speak(self):
+        if self.mood == "happy":
+            print("Purrr!")
+        elif self.mood == "angry":
+            print("Hiss!")
+        else:
+            print("Meow!")
+
+    def __init__(self):
+        self.mood = "neutral"
