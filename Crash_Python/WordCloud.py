570237a6fb8f0a10f87ed62819ee835abc718b70
Javier Chavez Manzo <javtychm@gmail.com>
2020-09-15 15:14:35 -0500

    Extrafiles

---
+++ Crash_Python/WordCloud.py
@@ -0,0 +1,10 @@
+# Problem Statement
+
+# The manager wants to create a daily report that tracks the use of machines, It is you job to create
+
+
+# Inputs - list of  Events
+# Objective - We need to process a list of Event objects using their attributes to generate a report that lists all users currently logged in to the machines.
+# Outputs - create a report that lists all the machine namses and for each machine, list of the users that are currently logged in
+# Event class, Contains the date when the event happened, the name of the machine where it happened, the user involved, and the event type
+# Atributes date, user, machine, type, The event type are string and we need to be care about are login and logout
